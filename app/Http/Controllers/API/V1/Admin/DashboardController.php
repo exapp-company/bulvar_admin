6ecc82828af8c6f035b9464f7db3fad1509385da
Samvel <asa0507790@gmail.com>
2024-12-04 11:15:58 +0400

    Обновил и удалил ненужные файлы

--- app/Http/Controllers/API/V1/Admin/DashboardController.php
+++ app/Http/Controllers/API/V1/Admin/DashboardController.php
@@ -2,14 +2,9 @@
 
 namespace App\Http\Controllers\API\V1\Admin;
 
-use App\Enums\HttpStatus;
 use App\Http\Controllers\ApiController;
-use App\Http\Requests\ShopRequest;
-use App\Http\Resources\Collections\ShopCollection;
-use App\Http\Resources\ShopResource;
-use App\Models\Shop;
+
 use App\Models\User;
-use App\Repositories\DashboardRepository;
 
 class DashboardController extends ApiController
 {
@@ -20,14 +15,9 @@ class DashboardController extends ApiController
     public function index()
     {
         $users = User::get();
-        $shops = Shop::groupBy('city_id');
 
         return [
             'users_count' => $users->count(),
-            'supplier_count' => $users->where('role', 'supplier')->count(),
-            'cities_count' => $shops->count(),
-            'chart_query' => [],
-            'top_query' => []
         ];
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,17 +2,11 @@
 
 namespace App\Providers;
 
-use App\Models\AdminUser;
-use App\Models\Office;
-use App\Models\Shop;
-use App\Models\User;
-use Illuminate\Auth\Notifications\ResetPassword;
-use Illuminate\Database\Eloquent\Relations\Relation;
+
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
-use MoonShine\Models\MoonshineUser;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,13 +22,6 @@ class AppServiceProvider extends ServiceProvider
         //                Log::debug('SQL', ['query' => $query->sql, 'bindings' => $query->bindings, 'time' => $query->time]);
         //            });
         //        }
-        Relation::enforceMorphMap([
-            'shop' => Shop::class,
-            'office' => Office::class,
-            'user' => User::class,
-            'muser' => MoonshineUser::class,
-            'admin' => AdminUser::class,
-        ]);
         JsonResource::wrap('items');
 
         // DB::listen(function ($query) {
@@ -44,11 +31,5 @@ class AppServiceProvider extends ServiceProvider
         //     );
         // });
 
-        //        resolve(EngineManager::class)->extend('elasticsearch', function () {
-        //            return new ElasticDriver(
-        //                ClientBuilder::create()->build(),
-        //                config('scout.elasticsearch.index')
-        //            );
-        //        });
     }
 }
