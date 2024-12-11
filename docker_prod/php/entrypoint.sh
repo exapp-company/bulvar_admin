8740287787d974f824d62dba9bff0203053ed07c
Samvel <asa0507790@gmail.com>
2024-12-11 15:13:18 +0400

    Убрал авто запуск миграции

--- docker_prod/php/entrypoint.sh
+++ docker_prod/php/entrypoint.sh
@@ -6,8 +6,7 @@ while ! mysql -h db -u"${DB_USERNAME}" -p"${DB_PASSWORD}" -e "SELECT 1" >/dev/nu
     sleep 1
 done
 
-php artisan migrate --force
-php artisan db:seed --force
+
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
